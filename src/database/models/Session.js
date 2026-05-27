b8937797e768656e06729f8cbfc4ea86f9bcc5cb
onlynewsao-cmyk <onlynewsao@gmail.com>
2026-05-27 11:24:23 -0700

    Delete src directory

--- src/database/models/Session.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Para persistir credenciais Baileys no MongoDB (não perder ao reiniciar Render)
-const SessionSchema = new mongoose.Schema({
-  fileName: { type: String, required: true, unique: true },
-  content: { type: String, required: true }, // JSON serializado
-}, { timestamps: true });
-
-module.exports = mongoose.model('Session', SessionSchema);
