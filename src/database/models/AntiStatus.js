52ff6e2963e1ffb1c75848eb69ce29542a797f59
onlynewsao-cmyk <onlynewsao@gmail.com>
2026-06-11 17:01:37 +0100

    Delete src/database/models directory

--- src/database/models/AntiStatus.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AntiStatusSchema = new mongoose.Schema({
-  groupJid: { type: String, required: true, unique: true },
-  enabled: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('AntiStatus', AntiStatusSchema);
